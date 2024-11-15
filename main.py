1a2e48a95ff4ac9f3a331e9801d24c6c5f14d3ba
Ahmed546 <ahmedwaleed0546@gmail.com>
2024-11-15 19:52:37 +0500

    Prepare-base-model completed

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModleTrainingPipeline
 STAGE_NAME="Data Ingestion Stage"
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<" )
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x" )
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME="Prepare Base Model Stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<" )
+        obj = PrepareBaseModleTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x" )
     except Exception as e:
